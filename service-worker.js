820c0bf868451f93c2b8cd04b250e15f2c8cfebb
PickNick68 <n.piccardi@alice.it>
2026-03-03 17:49:04 +0100

    update 147

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: oVAo85cK */
+/* Manifest version: h5+WmRC7 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
